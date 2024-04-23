c8b60c324c0837de01a86f19ede9ee8e653bfe47
hurma752 <hurma.zafar20@gmail.com>
2024-04-23 17:30:45 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number.", type: "number", name: "FirstNumber" },
+  { message: "Enter second number.", type: "number", name: "SecondNumber" },
+  {
+    message: "select the operation.",
+    type: "list",
+    name: "operation",
+    choices: ["Addition","Subtraction","Multiplication","Division"],
+  },
+]);
+
+if(answer.operation=== "Addition"){
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if(answer.operation=== "Subtraction"){
+    console.log( answer.FirstNumber - answer.SecondNumber);
+}
+else if(answer.operation=== "Multiplication"){
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if(answer.operation=== "Division"){
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else{
+    console.log("please select valid operator.")
+}
