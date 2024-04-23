d684e3eb4f96cb5907358c2718da8c828f1eae4e
hurma752 <hurma.zafar20@gmail.com>
2024-04-23 17:41:19 +0500

    added  console at the end

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number.", type: "number", name: "FirstNumber" },
-    { message: "Enter second number.", type: "number", name: "SecondNumber" },
+    { message: "Enter your first number.", type: "number", name: "FirstNumber" },
+    { message: "Enter your second number.", type: "number", name: "SecondNumber" },
     {
         message: "select the operation.",
         type: "list",
@@ -25,3 +25,4 @@ else if (answer.operation === "Division") {
 else {
     console.log("please select valid operator.");
 }
+console.log("The End!!!");
